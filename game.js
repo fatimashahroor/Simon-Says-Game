33d8d30e58a24082baa12db67b810d9509ff59e1
Fatima Shahrour <fatima.k.shahroor@gmail.com>
2024-06-30 04:44:32 +0300

    added reset game when choosing wrong button + added level up steps

--- game.js
+++ game.js
@@ -9,25 +9,59 @@ function nextSequence() {
     console.log(gamePattern);
 }
 
-function togglePressedClass () {
-    var lastElement= gamePattern[gamePattern.length-1];
-    var button=document.getElementById(lastElement);
+function togglePressedClass (pressedBtn) {
+    var button=document.getElementById(pressedBtn);
     button.classList.toggle("pressed");
 }
 
 function playGame() {
-    while (true) {
-        for (var i=0; i<gamePattern.length; i++) {
-            var lastElement= gamePattern[gamePattern.length-1];
-            var button=document.getElementById(lastElement);
-            var audio = new Audio(`../sounds/${lastElement}.mp3`);
-            button.classList.add("pressed");
-            audio.play();
-            setTimeout( () => {togglePressedClass()}, 200);
+    var lastElement= gamePattern[gamePattern.length-1];
+    var button=document.getElementById(lastElement);
+    var audio = new Audio(`../sounds/${lastElement}.mp3`);
+    button.classList.add("pressed");
+    console.log(button);
+    audio.play();
+    setTimeout( () => {togglePressedClass(lastElement)}, 50);
+    $(".btn").click(function () {
+        const userChosenColor = this.id;
+        this.classList.add("pressed");
+        new Audio (`../sounds/${userChosenColor}.mp3`).play();
+        setTimeout( function() {togglePressedClass(userChosenColor)}, 100);
+        console.log(userChosenColor);
+        userClickedPattern.push(userChosenColor);
+        console.log(userClickedPattern);
+        let answer= checkButton(userClickedPattern.length);
+        console.log(answer);
+        if (userClickedPattern.length == gamePattern.length && answer!= false) {
+            userClickedPattern=[];
+            level=level+1
+            document.querySelector("#level-title").textContent = `Level ${level}`;
+            setTimeout(nextSequence(), 3000);
+            $(".btn").unbind("click");
+            playGame();
         }
-        break;
+    });
+
+}
+
+
+function checkButton(index) {
+    if (gamePattern[index-1] != userClickedPattern[index-1]) {
+        $("h1").text("Game Over, Press Any Key To Restart");
+        $("body").addClass("game-over");
+        let wrong = new Audio("../sounds/wrong.mp3");
+        wrong.play();
+        setTimeout(function () {
+        $("body").removeClass("game-over");
+        }, 100);
+        gamePattern=[];
+        userClickedPattern=[];
+        $(".btn").unbind("click");
+        level=0;
+        started= false;
+        return false;
     }
-} 
+}
 
 function randomColor() {
     return Math.floor(Math.random()*(buttonColors.length));
@@ -35,7 +69,7 @@ function randomColor() {
 
 document.addEventListener('keypress', () => {
     if (!started) {
-        console.log(document.querySelector("#level-title").textContent = `Level ${level}`)
+        document.querySelector("#level-title").textContent = `Level ${level}`
         nextSequence();
         started = true
         playGame()
